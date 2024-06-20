a214734da0c582afa5f208e69a918392fdef0376
lgheric <saverneo@163.com>
2024-06-20 21:58:34 +0800

    init

---
+++ display-server-info.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Display Server Info
+Description: This plug-in will create a widget called "server information" in the admin dashboard,
+             and the PHP Version, MySQL Version, Server Software, Operating System and WordPress Version
+             will be displayed in this new widget.
+Version: 1.0
+Author: Codes4wp
+*/
+
+
+function dsi_add_dashboard_widgets() {
+    wp_add_dashboard_widget(
+        'dsi_dashboard_widget', // Widget slug.
+        'Server Information', // Title.
+        'dsi_display_server_info' // Display function.
+    );
+}
+add_action('wp_dashboard_setup', 'dsi_add_dashboard_widgets');
+
+function dsi_display_server_info() {
+	global $wp_version;		
+    echo '<p><strong>PHP Version:</strong> ' . PHP_VERSION . '</p>';
+    echo '<p><strong>MySQL Version:</strong> ' . $GLOBALS['wpdb']->db_version() . '</p>';
+    echo '<p><strong>Server Software:</strong> ' . $_SERVER['SERVER_SOFTWARE'] . '</p>';
+    echo '<p><strong>Operating System:</strong> ' . PHP_OS . '</p>';
+   	echo '<p><strong>WordPress Version:</strong> ' . $wp_version . '</p>';
+
+}
+
+

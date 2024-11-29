298173106d84e89cae7b0b00947642af064e2507
lgheric <saverneo@163.com>
2024-11-29 20:42:53 +0800

    fix bugs feedback from wordpress plugin directory team

--- display-server-info.php
+++ display-server-info.php
@@ -1,48 +1,79 @@
 <?php
-/*
-Plugin Name: Display Server Info
-Description: This plug-in will create a widget called "server information" in the admin dashboard,
+/**
+ * Plugin Name: Display Server Info
+ * Description: This plug-in will create a widget called "server information" in the admin dashboard,
              and the PHP Version, MySQL Version, Server Software, Operating System and WordPress Version
              will be displayed in this new widget.
-Version: 1.0
-Author: Robert South
+ * Version: 1.0
+ * Author: Robert South
+ * License: GPLv3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+
 */
 
+if ( !defined( 'ABSPATH' ) ) {
+    die( esc_html(__( "Can't load this file directly", 'display-server-info' ) ));
+}
 
-function dsi_add_dashboard_widgets() {
+// Plugin version
+if ( !defined( 'DISI_PLUGIN_VERSION' ) ) {
+    define( 'DISI_PLUGIN_VERSION', '1.0.0' );
+}
+
+// Plugin base name
+if ( !defined( 'DISI_PLUGIN_FILE' ) ) {
+    define( 'DISI_PLUGIN_FILE', __FILE__ );
+}
+
+// Plugin Folder Path
+if ( !defined( 'DISI_PLUGIN_DIR' ) ) {
+    define( 'DISI_PLUGIN_DIR', realpath( plugin_dir_path( DISI_PLUGIN_FILE ) ) . '/' );
+}
+
+$plugin_url = plugin_dir_url( DISI_PLUGIN_FILE );
+
+if ( is_ssl() ) {
+    $plugin_url = str_replace( 'http://', 'https://', $plugin_url );
+}
+if ( !defined( 'DISI_PLUGIN_URL' ) ) {
+    define( 'DISI_PLUGIN_URL', untrailingslashit( $plugin_url ) );
+}
+
+if ( !defined( 'DISI_ASSETS_URL' ) ) {
+    define( 'DISI_ASSETS_URL', DISI_PLUGIN_URL . '/assets' );
+}
+if ( !defined( 'DISI_JS_URL' ) ) {
+    define( 'DISI_JS_URL', DISI_ASSETS_URL . '/js' );
+}
+
+function disi_add_dashboard_widgets() {
     wp_add_dashboard_widget(
-        'dsi_dashboard_widget', // Widget slug.
+        'disi_dashboard_widget', // Widget slug.
         'Server Information', // Title.
-        'dsi_display_server_info' // Display function.
+        'disi_display_server_info' // Display function.
     );
     // Use a hook to move the widget
-    add_action('admin_footer', 'dsi_move_display_server_info_widget');
+    add_action('admin_footer', 'disi_move_display_server_info_widget');
 }
-add_action('wp_dashboard_setup', 'dsi_add_dashboard_widgets');
+add_action('wp_dashboard_setup', 'disi_add_dashboard_widgets');
 
-function dsi_display_server_info() {
+function disi_display_server_info() {
 
     //out put info
     echo '<p><strong>PHP Version:</strong> ' . esc_html(PHP_VERSION) . '</p>';
-    echo '<p><strong>MySQL Version:</strong> ' . esc_html($GLOBALS['wpdb']->db_version()) . '</p>';
-    echo '<p><strong>Server Software:</strong> ' . esc_html($_SERVER['SERVER_SOFTWARE']) . '</p>';
+    echo '<p><strong>MySQL Version:</strong> ' . esc_html(sanitize_text_field($GLOBALS['wpdb']->db_version())) . '</p>';
+    if(isset($_SERVER['SERVER_SOFTWARE'])){
+        echo '<p><strong>Server Software:</strong> ' . esc_html(sanitize_text_field(wp_unslash($_SERVER['SERVER_SOFTWARE']))) . '</p>';
+    }
     echo '<p><strong>Operating System:</strong> ' . esc_html(PHP_OS) . '</p>';
-
-    do_action( 'dsi_end_display_server_info');
+    do_action( 'disi_end_display_server_info');
 
 }
 
-function dsi_move_display_server_info_widget() {
-    echo "
-    <script type='text/javascript'>
-    jQuery(document).ready(function($) {
-        var displayServerInfoWidget = $('#dsi_dashboard_widget');
-        var atAGlance = $('#dashboard_right_now');
-
-        if (atAGlance.length && displayServerInfoWidget.length) {
-            displayServerInfoWidget.insertAfter(atAGlance);
-        }
-    });
-    </script>
-    ";
+function disi_move_display_server_info_widget() {
+    wp_register_script( 'disi-common-js', DISI_JS_URL . '/disi-common.min.js', array( 'jquery' ), DISI_PLUGIN_VERSION, true );
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'disi-common-js' );
 }
+add_action( 'admin_enqueue_scripts', 'disi_move_display_server_info_widget', 10 );
--- readme.txt
+++ readme.txt
@@ -3,10 +3,10 @@ Contributors: robert2021south
 Tags: display, server, info, dashboard, widget
 Donate link: http://ko-fi.com/robertsouth
 Requires at least: 5.6
-Tested up to: 6.5
+Tested up to: 6.7
 Requires PHP: 7.2
 Stable tag: 1.0
-License: GPLv3
+License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
 
@@ -40,16 +40,3 @@ If everything is all right, you will see the "Server Information" widget under t
 = 1.0 =
 * Initial version
 
-
-== PS ==
-I have worked for 10+ based on lamp/lnmp architecture, but I have never contributed any plug-ins to WordPress because I'm not very familiar with WordPress.
-When I lost my job, I thought of WordPress. I saw that many companies and individuals developed plug-ins for WordPress, and they were very popular with everyone. I want to look up to them.
-When I really wanted to develop a plug-in for WordPress, I was confused. I don't know what plug-in to develop, what the process was, and I don't even know what functions WordPress has and how to use it. It's funny, right?
-One day I accidentally searched for Display PHP Version developed by David Gwyer, and I thought it was time for me to get started.
-So I developed Display Server Info, a plug-in as simple as Display PHP Version.
-
-This is my first plug-in. Welcome to leave your valuable comments in the WordPress community. Thank you very much!
-I would like to express my gratitude to David Gwyer for giving me a hint and guiding me to get started.
-
-I am 45 years old this year, and I have decided to use the rest of my life to develop more simple and useful WordPress plug-ins.
-Will you support a novice developer like me?

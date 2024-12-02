cc067ce5b4055948655ad5dfc835aa3e4f2dc854
lgheric <saverneo@163.com>
2024-12-02 13:21:31 +0800

    update version number in top comment area

--- display-server-info.php
+++ display-server-info.php
@@ -4,7 +4,7 @@
  * Description: This plug-in will create a widget called "server information" in the admin dashboard,
              and the PHP Version, MySQL Version, Server Software, Operating System and WordPress Version
              will be displayed in this new widget.
- * Version: 1.0
+ * Version: 1.0.1
  * Author: Robert South
  * License: GPLv3 or later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
